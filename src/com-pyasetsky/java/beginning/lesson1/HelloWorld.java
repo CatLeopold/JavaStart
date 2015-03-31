8d19e29581df228b18f420df4c2a187f421a4b7d
CatLeopold <CatLeopold@users.noreply.github.com>
2015-03-31 13:51:03 +0300

    Create HelloWorld.java

---
+++ src/com-pyasetsky/java/beginning/lesson1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pyasetsky.java.beginning.lesson1;
+
+/**
+ * Created by bpyasetsky on 26.03.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}

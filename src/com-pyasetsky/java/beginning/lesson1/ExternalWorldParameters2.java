9a366719f812e2e00d1f718d9534e36928724e80
CatLeopold <CatLeopold@users.noreply.github.com>
2015-03-31 13:56:04 +0300

    Create ExternalWorldParameters2.java

---
+++ src/com-pyasetsky/java/beginning/lesson1/ExternalWorldParameters2.java
@@ -0,0 +1,10 @@
+package com.pyasetsky.java.beginning.lesson1;
+
+/**
+ * Created by bpyasetsky on 31.03.2015.
+ */
+public class ExternalWorldParameters2 {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("myproperty"));
+    }
+}
